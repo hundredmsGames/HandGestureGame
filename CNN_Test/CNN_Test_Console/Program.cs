4c806ef5d12e8e83cf6b344b91b821f7028b2208
Melih <mlhtncc@gmail.com>
2018-04-20 18:07:58 +0300

    Matrix Library and Fully Connected Neural Network added.

---
+++ CNN_Test/CNN_Test_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNN_Test_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello");
+            Console.ReadLine();
+        }
+    }
+}
