8bfe3c069092c283d433e672ed57d65772835be9
Melih <mlhtncc@gmail.com>
2018-04-27 21:18:49 +0300

    Problems :-1:

--- CNN_Test/CNN_Test_Console/CNN.cs
+++ CNN_Test/CNN_Test_Console/CNN.cs
@@ -8,6 +8,18 @@ using FullyConnectedNN;
 
 namespace ConvNeuralNetwork
 {
+    struct Location
+    {
+        public int r;
+        public int c;
+
+        public Location(int _r, int _c)
+        {
+            r = _r;
+            c = _c;
+        }
+    }
+
     class CNN
     {
         #region Configuration
@@ -37,6 +49,8 @@ namespace ConvNeuralNetwork
         // Max Pooling Map of layer2
         private Matrix m_pool1;
 
+        private List<Location> m_pool1_list;
+
         private Matrix relu1;
 
         private Matrix cnn_o;
@@ -61,8 +75,10 @@ namespace ConvNeuralNetwork
             //l1_w = new Matrix(l1_filter_rows, l1_filter_cols);
             //l1_w.Randomize();
 
+            m_pool1_list = new List<Location>();
+
             #region This will help while testing
-            
+
             Random r = new Random(12312324);
 
             this.input = new Matrix(8, 8);
@@ -132,7 +148,7 @@ namespace ConvNeuralNetwork
 
             f_map1 = new Matrix((r_w - f + 2 * p) / s + 1, (c_w - f + 2 * p) / s + 1);
 
-            Convolve(this.input, f_map1, l1_kernel, DotProduct, l1_kernel_size, l1_stride);
+            Convolve(this.input, f_map1, l1_kernel, null, DotProduct, l1_kernel_size, l1_stride);
 
             Console.WriteLine("Input\n");
             Console.WriteLine(this.input.ToString());
@@ -149,10 +165,14 @@ namespace ConvNeuralNetwork
 
             m_pool1 = new Matrix((r_w - f + 2 * p) / s + 1, (c_w - f + 2 * p) / s + 1);
 
-            Convolve(f_map1, m_pool1, null, MaxPooling, l2_kernel_size, l2_stride);
+            Convolve(f_map1, m_pool1, null, m_pool1_list, MaxPooling, l2_kernel_size, l2_stride);
 
             Console.WriteLine("\nMax Pooling1\n");
             Console.WriteLine(m_pool1.ToString());
+
+            Console.WriteLine("\nMax Pooling1 List\n");
+            foreach (Location l in m_pool1_list)
+                Console.WriteLine(l.r + ", " + l.c);
 
             relu1 = new Matrix(m_pool1.rows, m_pool1.cols);
             relu1 = Matrix.Map(m_pool1, ReLu);
@@ -178,45 +198,53 @@ namespace ConvNeuralNetwork
             Console.WriteLine(m_pool1_d_E.ToString());
 
 
-
-
-
-
             // f_map1__d__E = f_map1__d__m_pool1 * m_pool1__d__E
-
         }
 
 
 
-        private static void Convolve(Matrix input, Matrix output, Matrix kernel,
-            Func<Matrix, Matrix, int, int, int, double> func, int kernel_size, int stride)
+        private static void Convolve(Matrix input, Matrix output, Matrix kernel, List<Location> loc_list,
+            Func<Matrix, Matrix, List<Location>, int, int, int, double> func, int kernel_size, int stride)
         {
             for (int i = 0, r = 0; r < output.rows && i < input.rows; i += stride, r++)
             {
                 for (int j = 0, c = 0; c < output.cols && j < input.cols; j += stride, c++)
                 {
-                    output[r, c] = func(input, kernel, kernel_size, i, j);
+                    output[r, c] = func(input, kernel, loc_list, kernel_size, i, j);
                 }
             }
         }
 
+        //FIXME : We use list for location_list but we can use arrays too.
+        //using arrays would be a better solution
         // There is no kernel in max pooling so kernel is null.
-        private static double MaxPooling(Matrix input, Matrix kernel, int kernel_size, int rows, int cols)
+        private static double MaxPooling(Matrix input, Matrix kernel,
+            List<Location> loc_list, int kernel_size, int rows, int cols)
         {
             double max = double.MinValue;
+            int r = -1, c = -1;
 
-            for(int i = 0; i < kernel_size; i++)
+            for (int i = 0; i < kernel_size; i++)
             {
-                for(int j = 0; j < kernel_size; j++)
+                max = double.MinValue;
+                for (int j = 0; j < kernel_size; j++)
                 {
-                    max = Math.Max(max, input[i + rows, j + cols]);
+                    if (input[i + rows, j + cols] > max)
+                    {
+                        max = input[i + rows, j + cols];
+                        r = i + rows;
+                        c = j + cols;
+                    }
                 }
+
+                loc_list.Add(new Location(r, c));
             }
 
             return max;
         }
 
-        private static double DotProduct(Matrix input, Matrix kernel, int kernel_size, int rows, int cols)
+        private static double DotProduct(Matrix input, Matrix kernel,
+            List<Location> loc_list, int kernel_size, int rows, int cols)
         {
             double sum = 0.0f;
 
