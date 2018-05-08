37bf4844a9f6ffeb32e773df54033abdee652b8d
Melih <mlhtncc@gmail.com>
2018-05-09 00:31:20 +0300

    Bitmap to idx implemented

--- CNN_Test/CNN_Test/MainForm.cs
+++ CNN_Test/CNN_Test/MainForm.cs
@@ -14,10 +14,12 @@ namespace CNN_Test
         private const string captureString = "Capture";
         private const string captureStopString = "Stop";
 
-        private const int captureWidth  = 128;
+        private const int captureWidth = 128;
         private const int captureHeight = 128;
 
-        private static string desktopPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        private static string handImagesPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            "HandImages");
 
         private Capture capture;
         private bool captureState;
@@ -25,7 +27,10 @@ namespace CNN_Test
         public MainForm()
         {
             InitializeComponent();
-            
+
+            //BitmapToIdx();
+            //Read();
+
             InitializeCapture();
         }
 
@@ -65,9 +70,84 @@ namespace CNN_Test
                 captureBackWorker.WorkerReportsProgress = false;
 
                 Bitmap bmp = grayFrame.Bitmap;
-                bmp.Save(Path.Combine(desktopPath, "HandImages", (++counter) + ".png"), ImageFormat.Png);
+                bmp.Save(Path.Combine(handImagesPath, "Gesture", "1_" + (++counter) + ".png"), ImageFormat.Png);
             }
         }
+
+        private void BitmapToIdx()
+        {
+            string[] images = Directory.GetFiles(Path.Combine(handImagesPath, "Gesture"));
+
+            FileStream writeStream1 = new FileStream(Path.Combine(handImagesPath, "images.idx"), FileMode.Append);
+            FileStream writeStream2 = new FileStream(Path.Combine(handImagesPath, "labels.idx"), FileMode.Append);
+            {
+                BinaryWriter writeBinary1 = new BinaryWriter(writeStream1);
+                BinaryWriter writeBinary2 = new BinaryWriter(writeStream2);
+
+                Bitmap firstImage = new Bitmap(images[0]);
+
+                int magic = 126;
+                int numImages = images.Length;
+                int rows = firstImage.Width;
+                int cols = firstImage.Height;
+
+                writeBinary1.Write(magic);
+                writeBinary1.Write(numImages);
+                writeBinary1.Write(rows);
+                writeBinary1.Write(cols);
+
+                writeBinary2.Write(magic);
+                writeBinary2.Write(numImages);
+
+                foreach (string image in images)
+                {
+                    Bitmap bmp = new Bitmap(image);
+
+                    int label = image[image.LastIndexOf('\\') + 1] - '0';
+                    writeBinary2.Write((byte)label);
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            writeBinary1.Write(bmp.GetPixel(j, i).G);
+                        }
+                    }
+                }
+
+                writeBinary1.Close();
+                writeBinary2.Close();
+            }
+        }
+
+        #region This is for test purpose.
+        private void Read()
+        {
+            using (FileStream readStream = new FileStream(Path.Combine(handImagesPath, "images.idx"), FileMode.Open))
+            {
+                BinaryReader reader = new BinaryReader(readStream);
+
+                int magic1 = reader.ReadInt32();
+                int numImages = reader.ReadInt32();
+                int numRows = reader.ReadInt32();
+                int numCols = reader.ReadInt32();
+
+                Console.WriteLine(magic1);
+                Console.WriteLine(numImages);
+                Console.WriteLine(numRows);
+                Console.WriteLine(numCols);
+
+                for (int j = 0; j < numImages; j++)
+                {
+                    Console.WriteLine(reader.ReadByte());
+                    for (int i = 0; i < 128 * 128 - 1; i++)
+                        reader.ReadByte();
+                }
+
+                reader.Close();
+            }
+        }
+        #endregion
 
         #region Component Events
 
@@ -86,7 +166,7 @@ namespace CNN_Test
         {
             ToggleCaptureState();
         }
-        
+
         #endregion
     }
 }
