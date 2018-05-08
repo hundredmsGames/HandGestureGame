b10b1fb2a58368ccdd101ec1f2af068d652f1d02
Melih <mlhtncc@gmail.com>
2018-05-08 22:28:10 +0300

    Started to implement capture form

--- CNN_Test/CNN_Test/MainForm.Designer.cs
+++ CNN_Test/CNN_Test/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CNN_Test
 {
-    partial class PhotoCaptureForm
+    partial class MainForm
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -29,37 +29,44 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.captureBackWorker = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(104, 38);
+            this.pictureBox1.Location = new System.Drawing.Point(532, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(336, 244);
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // captureButton
             // 
-            this.button1.Location = new System.Drawing.Point(556, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.captureButton.Location = new System.Drawing.Point(660, 274);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(128, 36);
+            this.captureButton.TabIndex = 1;
+            this.captureButton.Text = "Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
             // 
-            // Form1
+            // captureBackWorker
+            // 
+            this.captureBackWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.DoWork);
+            this.captureBackWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.ProgressChanged);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.captureButton);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "CNN Test";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -68,7 +75,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button captureButton;
+        private System.ComponentModel.BackgroundWorker captureBackWorker;
     }
 }
 
---
+++ CNN_Test/CNN_Test/MainForm.cs
@@ -0,0 +1,92 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CNN_Test
+{
+    public partial class MainForm : Form
+    {
+        private const string captureString = "Capture";
+        private const string captureStopString = "Stop";
+
+        private const int captureWidth  = 128;
+        private const int captureHeight = 128;
+
+        private static string desktopPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+        private Capture capture;
+        private bool captureState;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            InitializeCapture();
+        }
+
+        private void InitializeCapture()
+        {
+            capture = new Capture();
+        }
+
+        private void ToggleCaptureState()
+        {
+            if (captureState == false)
+            {
+                captureState = true;
+                captureBackWorker.RunWorkerAsync();
+
+                captureButton.Text = captureStopString;
+            }
+            else
+            {
+                captureState = false;
+                capture.Pause();
+
+                captureButton.Text = captureString;
+
+            }
+        }
+
+        private void CaptureImage()
+        {
+            int counter = 0;
+            while (captureState == true)
+            {
+                Image<Gray, byte> grayFrame = capture.QueryGrayFrame().Resize(captureWidth, captureHeight, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                captureBackWorker.WorkerReportsProgress = true;
+                captureBackWorker.ReportProgress(0, grayFrame);
+                captureBackWorker.WorkerReportsProgress = false;
+
+                Bitmap bmp = grayFrame.Bitmap;
+                bmp.Save(Path.Combine(desktopPath, "HandImages", (++counter) + ".png"), ImageFormat.Png);
+            }
+        }
+
+        #region Component Events
+
+        public void DoWork(object obj, DoWorkEventArgs e)
+        {
+            CaptureImage();
+        }
+
+        public void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pictureBox1.Image = ((Image<Gray, byte>)e.UserState).Bitmap;
+            pictureBox1.Update();
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            ToggleCaptureState();
+        }
+        
+        #endregion
+    }
+}
