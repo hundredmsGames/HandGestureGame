e3553b75043a806e379fc77eca97b17192bcede0
SOFTSAM <sml.ozclk@gmail.com>
2018-05-15 22:39:17 +0300

    maxpooling design changed :zzz:

--- CNN_Test/CNN_Test_Console/CNN/Layers/MaxPoolingLayer.cs
+++ CNN_Test/CNN_Test_Console/CNN/Layers/MaxPoolingLayer.cs
@@ -9,7 +9,7 @@ namespace ConvNeuralNetwork
 
         private int kernel_size;
         private int stride;
-        List<Location> loc_list;
+        List<Location>[] loc_list;
         #endregion
 
         #region Constructors
@@ -18,7 +18,7 @@ namespace ConvNeuralNetwork
         {
             this.Kernel_Size = kernel_size;
             this.Stride = stride;
-            
+
         }
 
         #endregion
@@ -26,36 +26,44 @@ namespace ConvNeuralNetwork
 
         #region MaxPoolingMethod
 
-        private double MaxPooling()
+        public override void FeedForward()
         {
-            double max = double.MinValue;
+            base.FeedForward();
 
             //recreating everytime because we dont want to add more than we need
-            loc_list = new List<Location>();
-            for (int r = 0; r < Input.rows; r+=stride)
+            loc_list = new List<Location>[Input.Length];
+            for (int i = 0; i < loc_list.Length; i++)
             {
-                for (int c = 0; c < Input.cols; c+=stride)
+                loc_list[i] = new List<Location>();
+            }
+            for (int index = 0; index < Input.Length; index++)
+            {
+                int out_row_idx = 0, out_col_idx = 0;
+
+                for (int r = 0; r < Input[0].rows && out_row_idx < Output[0].rows; r += stride, out_row_idx++)
                 {
-                    for (int i = 0; i < this.Kernel_Size; i++)
+                    for (int c = 0; c < Input[0].cols && out_col_idx < Output[0].cols; c += stride, out_col_idx++)
                     {
-                        for (int j = 0; j < this.Kernel_Size; j++)
+                        double max = double.MinValue;
+                        for (int i = 0; i < this.Kernel_Size; i++)
                         {
-                            if (Input[i + r, j + c ] > max)
+                            for (int j = 0; j < this.Kernel_Size; j++)
                             {
-                                max = Input[i + r, j + c];
-                                r = i + r;
-                                c = j + c;
+                                if (Input[index][i + r, j + c] > max)
+                                {
+                                    max = Input[index][i + r, j + c];
+                                    r = i + r;
+                                    c = j + c;
+                                }
                             }
                         }
+                        loc_list[index].Add(new Location(r, c));
+                        Output[index][out_row_idx, out_col_idx] = max;
                     }
-                    
-                    loc_list.Add(new Location(r, c));
-                } 
+                   
+                }
             }
 
-            
-
-            return max;
         }
 
 
