922bf7d77a7c08863a24d0625c116904ccb2aaca
SOFTSAM <sml.ozclk@gmail.com>
2018-05-15 22:14:41 +0300

    maxpooling method

--- CNN_Test/CNN_Test_Console/CNN/Layers/MaxPoolingLayer.cs
+++ CNN_Test/CNN_Test_Console/CNN/Layers/MaxPoolingLayer.cs
@@ -1,4 +1,5 @@
 ﻿using MatrixLib;
+using System.Collections.Generic;
 
 namespace ConvNeuralNetwork
 {
@@ -8,7 +9,7 @@ namespace ConvNeuralNetwork
 
         private int kernel_size;
         private int stride;
-
+        List<Location> loc_list;
         #endregion
 
         #region Constructors
@@ -17,10 +18,48 @@ namespace ConvNeuralNetwork
         {
             this.Kernel_Size = kernel_size;
             this.Stride = stride;
+            
         }
 
         #endregion
 
+
+        #region MaxPoolingMethod
+
+        private double MaxPooling()
+        {
+            double max = double.MinValue;
+
+            //recreating everytime because we dont want to add more than we need
+            loc_list = new List<Location>();
+            for (int r = 0; r < Input.rows; r+=stride)
+            {
+                for (int c = 0; c < Input.cols; c+=stride)
+                {
+                    for (int i = 0; i < this.Kernel_Size; i++)
+                    {
+                        for (int j = 0; j < this.Kernel_Size; j++)
+                        {
+                            if (Input[i + r, j + c ] > max)
+                            {
+                                max = Input[i + r, j + c];
+                                r = i + r;
+                                c = j + c;
+                            }
+                        }
+                    }
+                    
+                    loc_list.Add(new Location(r, c));
+                } 
+            }
+
+            
+
+            return max;
+        }
+
+
+        #endregion
         #region Properties
 
         public int Kernel_Size
