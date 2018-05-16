79cebd4da9bcd45e6ef6e4edb488e974372b83df
SOFTSAM <sml.ozclk@gmail.com>
2018-05-16 16:11:43 +0300

    structures class added

---
+++ CNN_Test/CNN_Test_Console/CNN/Structures.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvNeuralNetwork
+{
+    struct Location
+    {
+        public int r;
+        public int c;
+
+        public Location(int _r, int _c)
+        {
+            r = _r;
+            c = _c;
+        }
+    }
+    struct LayerDescription
+    {
+        //input desc
+        public int width;
+        public int height;
+        public int channels;
+
+        //conv & pool desc
+        public int kernel_size;
+        public int stride;
+        public int padding;
+
+        //fcnn
+        public int hiddenNeuronsCount;
+        public int inputNeuronsCount;
+        public int outputNeuronsCount;
+        public float learningRate;
+
+        public Func<double, double> activationFunc;
+        public Func<double, double> derofActivationFunc;
+
+
+        public LayerType layerType;
+
+    }
+}
