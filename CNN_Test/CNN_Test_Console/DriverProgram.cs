e7724e8499209f573a8470937eed4e5b1a3c9455
SOFTSAM <sml.ozclk@gmail.com>
2018-06-11 17:15:38 +0300

    interface changed
    now we are testing for every epoch to see what is the accuracy and how it effects for more training

--- CNN_Test/CNN_Test_Console/DriverProgram.cs
+++ CNN_Test/CNN_Test_Console/DriverProgram.cs
@@ -9,7 +9,7 @@ namespace CNN_Test_Console
 {
     class DriverProgram
     {
-        static int cursorTop = 0;
+        static int cursorTopTraining = -1, cursorTopTesting = 0;
 
         static void Main(string[] args)
         {
@@ -21,15 +21,16 @@ namespace CNN_Test_Console
 
         public static void CNN_Test()
         {
-            int trCount = 100, tsCount = 100;
-            double error = 0f;
-            double timeLimit = 0;
-            int iterationCount = 20;
+            int trCount = 60000, tsCount = 10000;
+            double error = 0f, timeLimit = 0f;
+
+            int iterationCount = 10;
             bool predictionIsOn = true;
             Random random = new Random();
 
-            DigitImage[] digitImagesDatas = MNIST_Parser.ReadFromFile(DataSet.Training, trCount);
-            int training_count = digitImagesDatas.Length;
+            DigitImage[] trainigDigitImagesDatas = MNIST_Parser.ReadFromFile(DataSet.Training, trCount);
+            DigitImage[] testingDigitImagesDatas = MNIST_Parser.ReadFromFile(DataSet.Testing, tsCount);
+            int training_count = trainigDigitImagesDatas.Length;
             int dialogResult;
 
             CNN cnn = CreateCNN(out dialogResult);
@@ -42,10 +43,12 @@ namespace CNN_Test_Console
             if (dialogResult == 0)
             {
                 Console.WriteLine("System is getting trained...");
-                cursorTop = Console.CursorTop;
+                //if we never assigned this assign only once
+                if (cursorTopTraining == -1)
+                    cursorTopTraining = Console.CursorTop;
                 for (int x = 0; x < iterationCount; x++)
                 {
-                    DigitImage[] digitImages = digitImagesDatas.OrderBy(image => random.Next(training_count)).ToArray();
+                    DigitImage[] digitImages = trainigDigitImagesDatas.OrderBy(image => random.Next(training_count)).ToArray();
                     for (int i = 0; i < training_count; i++)
                     {
                         for (int j = 0; j < 28; j++)
@@ -63,18 +66,50 @@ namespace CNN_Test_Console
                         }
 
                         int val = Map(0, training_count * iterationCount, 0, 100, training_count * x + i);
-                        ProgressBar(val, training_count * x + i, training_count * iterationCount, error, stopwatch.ElapsedMilliseconds / 1000.0);
+                        ProgressBar(val, training_count * x + i, training_count * iterationCount, error, stopwatch.ElapsedMilliseconds / 1000.0, cursorTopTraining);
                     }
+                    SystemTestCode(testingDigitImagesDatas, predictionIsOn, cnn, x + 1);
                 }
 
                 Console.WriteLine("\nSystem has been trained.");
             }
 
-            digitImagesDatas = MNIST_Parser.ReadFromFile(DataSet.Testing, tsCount);
+
+            //if we are not training the system we dont have to save again
+            if (dialogResult == 0)
+            {
+                Console.WriteLine("Would you like to save this network?(yY/nN)");
+                string res = Console.ReadLine();
+
+                if (res == "" || char.ToLower(res[0]) == 'y')
+                {
+                    Console.WriteLine("Press Enter to continue, if you do not write a name, there will be a default name.");
+                    Console.Write("File Name >> ");
+                    string fileName = Console.ReadLine().Trim();
+                    cnn.SaveData(fileName);
+                }
+                Console.WriteLine("Data Saved...");
+            }
+
+            Console.WriteLine("Press enter to exit.");
+        }
+        public static void SystemTestCode(DigitImage[] digitImagesDatas, bool predictionIsOn, CNN cnn, int iterationCount)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             int testing_count = digitImagesDatas.Length;
             int correct_count = 0;
+            double timeLimit = 0, error = 0;
+            Matrix[] input, targets;
+
             Console.WriteLine("System is getting tested. You will see the results when it is done...\n");
-            cursorTop = Console.CursorTop;
+
+
+            if (cursorTopTesting == 0)
+                cursorTopTesting = Console.CursorTop;
+            InitializeInputAndTarget(out input, out targets);
+
             timeLimit = stopwatch.ElapsedMilliseconds;
 
             for (int i = 0; i < testing_count; i++)
@@ -105,132 +140,131 @@ namespace CNN_Test_Console
                 }
 
                 int val = Map(0, testing_count, 0, 100, i);
-                ProgressBar(val, i, testing_count, error, stopwatch.ElapsedMilliseconds / 1000.0);
+                ProgressBar(val, i, testing_count, error, stopwatch.ElapsedMilliseconds / 1000.0, cursorTopTesting);
             }
+            double accuracy = (correct_count * 1f / testing_count) * 100.0;
 
+            Console.WriteLine("\nIteration Count:" + iterationCount);
             Console.WriteLine("\nTime :" + (stopwatch.ElapsedMilliseconds / 1000.0).ToString("F4"));
-            Console.WriteLine("\nAccuracy: %{0:F2}\n", (correct_count * 1f / testing_count) * 100.0);
+            Console.WriteLine("\nAccuracy: %{0:F2}\n", accuracy);
             Console.WriteLine("Correct/All: {0}/{1}", correct_count, testing_count);
 
-            //if we are not training the system we dont have to save again
-            if (dialogResult == 0)
-            {
-                Console.WriteLine("Would you like to save this network?(yY/nN)");
-                string res = Console.ReadLine();
+            cursorTopTesting = Console.CursorTop;
 
-                if (res == "" || char.ToLower(res[0]) == 'y')
+            if (accuracy >= 95)
+            {
+                string name = accuracy.ToString("F2") + " " + iterationCount + "";
+                Random random = new Random();
+                int length = random.Next(3, 6);
+                for (int i = 0; i < length; i++)
                 {
-                    Console.WriteLine("Press Enter to continue, if you do not write a name, there will be a default name.");
-                    Console.Write("File Name >> ");
-                    string fileName = Console.ReadLine().Trim();
-                    cnn.SaveData(fileName);
+                    char c = (char)random.Next(65, 122);
+                    name += c;
                 }
-                Console.WriteLine("Data Saved...");
+                cnn.SaveData(name);
             }
 
-            Console.WriteLine("Press enter to exit.");
-        }
-
-        public static void CNN_OverfittingTest()
-        {
-            CNN cnn = new CNN();
-
-            DigitImage[] digitImages = MNIST_Parser.ReadFromFile(DataSet.Testing, 100);
-
-            int test_image_idx = 5;
-            Matrix[] input = new Matrix[1];
-            input[0] = new Matrix(digitImages[test_image_idx].pixels);
-            Matrix target = new Matrix(10, 1);
-            target[(int)digitImages[test_image_idx].label, 0] = 1f;
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            int iteration_count = 1000;
-            for (int i = 0; i < iteration_count; i++)
-            {
-                cnn.Train(input, target);
-                double error = cnn.GetError();
-
-                int val = (int)((i - 0) / (double)(iteration_count - 1 - 0) * (100 - 0) + 0);
-                ProgressBar(val, i, iteration_count, error, stopwatch.ElapsedMilliseconds / 1000.0);
             }
-
-            Matrix output = cnn.Predict(input);
-
-            Console.WriteLine(output.ToString());
-            Console.WriteLine(digitImages[test_image_idx].ToString());
-        }
-
-
-
-        public static CNN CreateCNN(out int dialogResult)
-        {
-            string[] files = Directory.GetFiles(Path.Combine("..", "..", "CNN", "Configs"), "*.json");
-
-            Console.WriteLine("0 > Train new network");
-            for (int i = 0; i < files.Length; i++)
+            public static void CNN_OverfittingTest()
             {
-                Console.WriteLine("{0} > {1}", i + 1, files[i].Substring(files[i].LastIndexOf(Path.DirectorySeparatorChar) + 1));
-            }
-            Console.Write(">> ");
+                CNN cnn = new CNN();
 
-            int indexOfFile = int.Parse(Console.ReadLine().Trim());
-            dialogResult = indexOfFile;
-            if (indexOfFile != 0 && indexOfFile < files.Length)
-                return new CNN(files[indexOfFile - 1]);
-            else
-                return new CNN();
-        }
+                DigitImage[] digitImages = MNIST_Parser.ReadFromFile(DataSet.Testing, 100);
 
-        public static void InitializeInputAndTarget(out Matrix[] input, out Matrix[] targets)
-        {
-            input = new Matrix[1];
-            input[0] = new Matrix(28, 28);
-            targets = new Matrix[10];
+                int test_image_idx = 5;
+                Matrix[] input = new Matrix[1];
+                input[0] = new Matrix(digitImages[test_image_idx].pixels);
+                Matrix target = new Matrix(10, 1);
+                target[(int)digitImages[test_image_idx].label, 0] = 1f;
 
-            for (int i = 0; i < 10; i++)
-            {
-                targets[i] = new Matrix(10, 1);
-                for (int j = 0; j < 10; j++)
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int iteration_count = 1000;
+                for (int i = 0; i < iteration_count; i++)
                 {
-                    targets[i][j, 0] = (i == j) ? 1.0f : 0.0f;
+                    cnn.Train(input, target);
+                    double error = cnn.GetError();
+
+                    int val = (int)((i - 0) / (double)(iteration_count - 1 - 0) * (100 - 0) + 0);
+                    ProgressBar(val, i, iteration_count, error, stopwatch.ElapsedMilliseconds / 1000.0);
+                }
+
+                Matrix output = cnn.Predict(input);
+
+                Console.WriteLine(output.ToString());
+                Console.WriteLine(digitImages[test_image_idx].ToString());
+            }
+
+
+
+            public static CNN CreateCNN(out int dialogResult)
+            {
+                string[] files = Directory.GetFiles(Path.Combine("..", "..", "CNN", "Configs"), "*.json");
+
+                Console.WriteLine("0 > Train new network");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Console.WriteLine("{0} > {1}", i + 1, files[i].Substring(files[i].LastIndexOf(Path.DirectorySeparatorChar) + 1));
+                }
+                Console.Write(">> ");
+
+                int indexOfFile = int.Parse(Console.ReadLine().Trim());
+                dialogResult = indexOfFile;
+                if (indexOfFile != 0 && indexOfFile < files.Length)
+                    return new CNN(files[indexOfFile - 1]);
+                else
+                    return new CNN();
+            }
+
+            public static void InitializeInputAndTarget(out Matrix[] input, out Matrix[] targets)
+            {
+                input = new Matrix[1];
+                input[0] = new Matrix(28, 28);
+                targets = new Matrix[10];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    targets[i] = new Matrix(10, 1);
+                    for (int j = 0; j < 10; j++)
+                    {
+                        targets[i][j, 0] = (i == j) ? 1.0f : 0.0f;
+                    }
                 }
             }
-        }
 
-        public static int Map(int oldMin, int oldMax, int newMin, int newMax, int current)
-        {
-            //Y = (X-A)/(B-A) * (D-C) + C
-            return (int)((current - oldMin) / (double)(oldMax - 1 - oldMin) * (newMax - newMin) + newMin);
-        }
-
-        static void ProgressBar(int currentValue, int currentCount, int maxCount, double error, double timePassed = 0)
-        {
-            Console.CursorVisible = false;
-
-            int pos = currentValue / 10;
-            if (currentValue == 0)
+            public static int Map(int oldMin, int oldMax, int newMin, int newMax, int current)
             {
-                Console.SetCursorPosition(0, cursorTop);
-                Console.Write("[");
-                Console.SetCursorPosition(pos + 12, cursorTop);
-                Console.Write("]");
+                //Y = (X-A)/(B-A) * (D-C) + C
+                return (int)((current - oldMin) / (double)(oldMax - 1 - oldMin) * (newMax - newMin) + newMin);
             }
 
-            Console.SetCursorPosition(pos + 1, cursorTop);
-            Console.Write("#");
-            Console.SetCursorPosition(14, cursorTop);
-            Console.WriteLine(currentValue + "%");
-            Console.SetCursorPosition(25, cursorTop);
-            Console.WriteLine(currentCount + 1 + " / " + maxCount);
-            Console.SetCursorPosition(45, cursorTop);
-            Console.WriteLine("Time Passed: " + timePassed.ToString("F1"));
-            Console.SetCursorPosition(70, cursorTop);
-            Console.WriteLine("Error: {0:F6}", error);
+            static void ProgressBar(int currentValue, int currentCount, int maxCount, double error, double timePassed = 0, int cursorTop = 0)
+            {
+                Console.CursorVisible = false;
 
-            Console.CursorVisible = true;
+                int pos = currentValue / 10;
+                if (currentValue == 0)
+                {
+                    Console.SetCursorPosition(0, cursorTop);
+                    Console.Write("[");
+                    Console.SetCursorPosition(pos + 12, cursorTop);
+                    Console.Write("]");
+                }
+
+                Console.SetCursorPosition(pos + 1, cursorTop);
+                Console.Write("#");
+                Console.SetCursorPosition(14, cursorTop);
+                Console.WriteLine(currentValue + "%");
+                Console.SetCursorPosition(25, cursorTop);
+                Console.WriteLine(currentCount + 1 + " / " + maxCount);
+                Console.SetCursorPosition(45, cursorTop);
+                Console.WriteLine("Time Passed: " + timePassed.ToString("F1"));
+                Console.SetCursorPosition(70, cursorTop);
+                Console.WriteLine("Error: {0:F6}", error);
+
+                Console.CursorVisible = true;
+            }
         }
     }
-}
 
